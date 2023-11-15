287b37c5d498e9031834ce0d0dc216c807704bc0
Srinivas Sai <srinibash.sai26@gmail.com>
2023-11-15 23:33:40 +0530

    added the layout and code for pass a value from one activity to another

--- app/src/main/java/com/example/intentactivity/SecondActivity.kt
+++ app/src/main/java/com/example/intentactivity/SecondActivity.kt
@@ -1,12 +1,24 @@
 package com.example.intentactivity
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        //accept that value from the intent and print that value through a text view
+
+        lateinit var tvName : TextView
+        lateinit var tvRoll : TextView
+
+        tvName = findViewById(R.id.name)
+        tvRoll= findViewById(R.id.rollno)
+
+        val bundle = intent.extras
+        if (bundle != null){
+            tvName.text = "Name = ${bundle.getString("name")}"
+            tvRoll.text = "RollNo = ${bundle.getString("roll")}"
+        }
     }
 }
